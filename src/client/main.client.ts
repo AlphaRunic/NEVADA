bbe9e41b8bedb9b5a21d181d5680519c3354a2d9
AlphaRunic <peenreee@gmail.com>
2021-05-16 09:17:53 +0100

    Add stamina and health bar UX

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,10 +1,12 @@
 import { Carbon } from "shared/Carbon/Framework";
 import { CameraAnimation } from "./GameComponents/CameraAnimation";
 import { Movement } from "./GameComponents/Movement";
+import { HealthUX } from "./GameComponents/UX/HealthUX";
 import { StaminaUX } from "./GameComponents/UX/StaminaUX";
 
 Carbon.RunComponents(
     new Movement,
     new CameraAnimation,
-    new StaminaUX
+    new StaminaUX,
+    new HealthUX
 );
